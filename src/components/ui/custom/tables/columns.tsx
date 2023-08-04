08ec0c1e9cb9d26b0c60db39b657e2232d6a08b8
ross-patiak <ross.patiak@gmail.com>
2023-08-04 16:24:43 -0400

    building blocks for tables

---
+++ src/components/ui/custom/tables/columns.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { type ColumnDef } from "@tanstack/react-table";
+import { type Employee } from "@prisma/client";
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+// export type Payment = {
+//   id: string;
+//   amount: number;
+//   status: "pending" | "processing" | "success" | "failed";
+//   email: string;
+// };
+
+export const employeeListColumns: ColumnDef<Employee>[] = [
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
+  },
+];
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,14 +1,21 @@
 import CreateUserButton from "@/components/ui/custom/CreateUserButton";
+import EmployeesList from "@/components/ui/custom/EmployeesList";
+import { employeeListColumns } from "@/components/ui/custom/tables/columns";
+import { api } from "@/utils/api";
+import { type Employee } from "@prisma/client";
 
 import { type NextPage } from "next";
 
-const TicketsPage: NextPage = () => {
-  //TODO: make title, content, etc required/optional
+const UsersPage: NextPage = () => {
+  const getEmployees = api.employee.getEmployees.useQuery();
+  const data: Employee[] = getEmployees.data as Employee[];
+
   return (
     <div className="flex">
       <CreateUserButton className="self-end" />
+      <EmployeesList data={data} columns={employeeListColumns} />
     </div>
   );
 };
 
-export default TicketsPage;
+export default UsersPage;
