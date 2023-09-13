657ca2efa0273f28fcfdc0141b15da212d3a5846
ross-patiak <ross.patiak@gmail.com>
2023-09-12 20:17:59 -0400

    added basic search func to tickets+users

--- src/components/ui/custom/tickets/TicketsList.tsx
+++ src/components/ui/custom/tickets/TicketsList.tsx
@@ -4,12 +4,29 @@ import { TextField } from "@radix-ui/themes";
 import TicketsListEntry from "@/components/ui/custom/tickets/TicketsListEntry";
 import { Search } from "lucide-react";
 import { type ticketWithEmployeeType } from "@/lib/prismaTypes";
+import { useEffect, useState } from "react";
+import { api } from "@/utils/api";
 
 type TicketsListProps = {
   data: ticketWithEmployeeType[];
 };
 
 const TicketsList = ({ data }: TicketsListProps) => {
+  const [dataToShow, setDataToShow] = useState(data);
+  const [inputToSearch, setInputToSearch] = useState("");
+  const getTicketsByTitle = api.ticket.getTicketsByTitle.useQuery({
+    input: inputToSearch,
+  });
+
+  useEffect(() => {
+    const filteredData = getTicketsByTitle?.data as ticketWithEmployeeType[];
+    if (filteredData?.length > 0) {
+      setDataToShow(filteredData);
+    } else {
+      setDataToShow(data);
+    }
+  }, [data, getTicketsByTitle?.data]);
+
   return (
     <div className="flex flex-col gap-4">
       <div className="self-end">
@@ -17,12 +34,15 @@ const TicketsList = ({ data }: TicketsListProps) => {
           <TextField.Slot>
             <Search size={16} />
           </TextField.Slot>
-          <TextField.Input placeholder="Search tickets" />
+          <TextField.Input
+            placeholder="Search tickets"
+            onChange={(e) => setInputToSearch(e.target.value)}
+          />
         </TextField.Root>
       </div>
 
       <div className="grid grid-cols-4 gap-x-4 gap-y-4">
-        {data.map((ticket) => (
+        {dataToShow.map((ticket) => (
           <TicketsListEntry key={ticket?.id} ticket={ticket} />
         ))}
       </div>
--- src/components/ui/custom/users/EmployeesList.tsx
+++ src/components/ui/custom/users/EmployeesList.tsx
@@ -4,12 +4,29 @@ import { TextField } from "@radix-ui/themes";
 import EmployeesListEntry from "@/components/ui/custom/users/EmployeesListEntry";
 import { Search } from "lucide-react";
 import { type employeesWithTicketsType } from "@/lib/prismaTypes";
+import { useEffect, useState } from "react";
+import { api } from "@/utils/api";
 
 type EmployeesListProps = {
   data: employeesWithTicketsType[];
 };
 
 const EmployeesList = ({ data }: EmployeesListProps) => {
+  const [dataToShow, setDataToShow] = useState(data);
+  const [inputToSearch, setInputToSearch] = useState("");
+  const getEmployeesByName = api.employee.getEmployeesByName.useQuery({
+    input: inputToSearch,
+  });
+
+  useEffect(() => {
+    const filteredData = getEmployeesByName?.data as employeesWithTicketsType[];
+    if (filteredData?.length > 0) {
+      setDataToShow(filteredData);
+    } else {
+      setDataToShow(data);
+    }
+  }, [data, getEmployeesByName?.data]);
+
   return (
     <div className="flex flex-col gap-4">
       <div className="self-end">
@@ -17,12 +34,15 @@ const EmployeesList = ({ data }: EmployeesListProps) => {
           <TextField.Slot>
             <Search size={16} />
           </TextField.Slot>
-          <TextField.Input placeholder="Search users" />
+          <TextField.Input
+            placeholder="Search users"
+            onChange={(e) => setInputToSearch(e.target.value)}
+          />
         </TextField.Root>
       </div>
 
       <div className="grid grid-cols-3 gap-x-4 gap-y-4">
-        {data.map((employee) => (
+        {dataToShow.map((employee) => (
           <EmployeesListEntry key={employee?.id} employee={employee} />
         ))}
       </div>
